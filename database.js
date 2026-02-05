b539f7d7b955e3c6fdff4d9170036d012a967706
yassin <ye2300080@tkh.edu.eg>
2026-02-05 17:23:40 +0200

    student and staff schema

--- database.js
+++ database.js
@@ -7,6 +7,15 @@ const db = new sqlite3.Database('./flashedu.db', (err) => {
     if (err) console.error('Database error:', err.message);
     else {
         console.log('connected to the database');
+
+        //create users and classrooms table for students and staff
+        db.run(`CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role TEXT NOT NULL CHECK(role IN ('Student', 'Staff'))
+        )`);
     }
 });
 
