cadc4ac59d82e7b87b85a0cbded09e28364bf2bc
yassin <ye2300080@tkh.edu.eg>
2026-02-06 00:51:30 +0200

    added user validation and hashing passwords

--- controller/authController.js
+++ controller/authController.js
@@ -4,11 +4,26 @@ const db = require('../database.js');
 const bcrypt = require('bcryptjs'); 
 
 //handle user registration
-const register = (req, res) => {
-    const { name, email, password, role } = req.body;
+const { name, email, password, role } = req.body;
 
     if (!name || !email || !password || !role) {
-        return res.status(400).json({ error: "Please provide all fields!" });
+        return res.status(400).json({ error: "Missing required fields" });
     }
+//Hash the password 
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+            return res.status(500).json({ error: "Error securing password" });
+        }
 
+        //Insert new user into the database
+        const sql = `INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)`;
+        const params = [name, email, hashedPassword, role];
+
+        db.run(sql, params, function(err) {
+            if (err) {
+                return res.status(400).json({ error: "Email already exists or error occured" });
+            }
+            res.status(201).json({ message: "User registered successfully!" });
+        });
+    });
 module.exports = { register };
