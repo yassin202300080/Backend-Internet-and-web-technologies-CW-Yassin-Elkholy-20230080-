68d0383e492198173e02682f310b34c0b1812e30
yassin <ye2300080@tkh.edu.eg>
2026-02-05 18:14:24 +0200

    add middleware and db connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+//middleware
+app.use(express.json());
+
+const db = require('./database');
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
